7503c102a7520c5164b157519a3b4e7977619919
matheush-a <mathzalmeida@gmail.com>
2024-02-02 12:09:14 -0300

    feat: importação de módulos no módulo principal da aplicação

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ModalidadeModule } from './app/modalidade/modalidade.module';
+import { TipoCursoModule } from './app/tipoCurso/tipoCurso.module';
+import { CursoModule } from './app/curso/curso.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    CursoModule,
+    ModalidadeModule,
+    TipoCursoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
