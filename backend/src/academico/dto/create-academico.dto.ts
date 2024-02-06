053a82ac565685e64f61828640a8dcd7c4588b6e
matheush-a <mathzalmeida@gmail.com>
2024-02-06 14:49:07 -0300

    feat: validação inserida nos campos dos dtos de acadêmico

--- backend/src/academico/dto/create-academico.dto.ts
+++ backend/src/academico/dto/create-academico.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsNotEmpty, IsNumberString, Validate } from "class-validator";
+import { IsPhone } from "../validation/academico.validator";
+import { Academico } from "../entities/academico.entity";
 
 export class CreateAcademicoDto {
+  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
   nome: string;
+
+  @IsEmail({}, { message: 'O e-mail informado é inválido'})
   email: string;
+
+  @Validate(IsPhone)
   telefone: string;
+
+  @IsNumberString()
+  cursoId: number;
 }
--- backend/src/academico/dto/update-academico.dto.ts
+++ backend/src/academico/dto/update-academico.dto.ts
@@ -1,9 +1,26 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAcademicoDto } from './create-academico.dto';
+import { IsDateString, IsNumberString, IsOptional, IsString, Length, Validate } from 'class-validator';
+import { IsCEP } from '../validation/academico.validator';
 
 export class UpdateAcademicoDto extends PartialType(CreateAcademicoDto) {
+  @IsOptional()
+  @Validate(IsCEP)
   cep?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(11)
   cpf?: string;
-  dt_nascimento?: Date;
+
+  @IsOptional()
+  @IsDateString()
+  dt_nascimento?: string;
+
+  @IsOptional()
   documento?: string;
+
+  @IsOptional()
+  @IsNumberString()
+  tipoDocumentoId: number;
 }
