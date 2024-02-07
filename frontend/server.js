61c6a46299148e7f17bcd4fd8ad2898c9cfd7497
matheush-a <mathzalmeida@gmail.com>
2024-02-07 20:51:05 -0300

    fix: linhas faltantes no server.js

--- frontend/server.js
+++ frontend/server.js
@@ -9,3 +9,7 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')));
 app.get(/.*/, function(req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 });
+
+const port = process.env.PORT || 8080;
+
+app.listen(port);
