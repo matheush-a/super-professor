bc030c2ea782ad64863c20b5f149212b0fcf0969
matheush-a <mathzalmeida@gmail.com>
2024-02-07 12:00:17 -0300

    fix: cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   
   await app.listen(3000);
   console.log('Server is running @ http://localhost:3000');
