691fcff2a3b34cd58b6f30f6b5440b47898e50c2
matheush-a <mathzalmeida@gmail.com>
2024-02-07 16:57:10 -0300

    feat: aplicação principal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,44 +3,95 @@ import axios from 'axios';
 import FirstStep from './components/matricula/FirstStep.vue';
 import SecondStep from './components/matricula/SecondStep.vue';
 import ThirdStep from './components/matricula/ThirdStep.vue';
+import SnackBar from './components/SnackBar.vue';
 
 export default {
   data() {
     return {
+      errors: [],
       stepActive: 1,
       createAcademico: {
         nome: '',
         email: '',
         telefone: '',
         cursoId: '',
-      }
+      },
+      updateAcademico: {
+        documento: '',
+        tipoDocumento: '',
+        cep: '',
+        cpf: '',
+        dt_nascimento: '',
+      },
+      academico: {}
     }
   },
   components: {
     FirstStep,
     SecondStep,
-    ThirdStep
+    ThirdStep,
+    SnackBar,
   },
   methods: {
     setCurrentStep(stepNumber) {
       this.stepActive = stepNumber;
     },
-    fillAcademico(academico, create = false) {
+    fillCreateAcademico(academico, create = false) {
       Object.assign(this.createAcademico, academico);
 
       if(create) {
         this.apiPostAcademico();
       }
     },
+    fillUpdateAcademico(academico) {
+      Object.assign(this.updateAcademico, academico);
+
+      this.apiUpdateAcademico();
+    },
     async apiPostAcademico() {
+      this.errors = [];
+
       await axios.post('http://localhost:3000/academico', this.createAcademico)
         .then(response => {
-          this.cursos = response.data;
-          this.filteredCursos = response.data;
+          this.academico = response.data
+          this.stepActive++;
         })
         .catch(error => {
-          this.error = error.message;
+          if (error.response) {
+            error.response.data.message.forEach((item) => {
+              this.errors.push(item);
+            })
+          } else {
+            this.errors = ["Ocorreu um erro inesperado."]
+          }
         });
+    },
+    async apiUpdateAcademico() {
+      let formData = new FormData();
+      formData.append('documento', this.updateAcademico.documento);
+      formData.append('tipoDocumento', this.updateAcademico.tipoDocumento);
+      formData.append('cep', this.updateAcademico.cep);
+      formData.append('cpf', this.updateAcademico.cpf);
+      formData.append('dt_nascimento', this.updateAcademico.dt_nascimento);
+
+      this.errors = [];
+      await axios.patch(`http://localhost:3000/academico/${this.academico.id}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(response => {
+        this.stepActive = 1;
+      })
+      .catch(error => {
+        if (error.response) {
+          error.response.data.message.forEach(async (item) => {
+            this.errors.push(item);
+          })
+        } else {
+          this.errors = ["Ocorreu um erro inesperado."]
+        }
+      });
     },
   }
 }
@@ -48,10 +99,11 @@ export default {
 
 <template>
   <div>
+    <SnackBar v-if="errors.length" :messages="errors"/>
     <img src="/matriculas.png" class="logo" alt="Matríclas logo" />
-    <FirstStep v-if="stepActive === 1" @setCurrentStep="setCurrentStep" @fillAcademico="fillAcademico"/>
-    <SecondStep v-if="stepActive === 2" @setCurrentStep="setCurrentStep" @fillAcademico="fillAcademico"/>
-    <ThirdStep v-if="stepActive === 3" @setCurrentStep="setCurrentStep"/>
+    <FirstStep v-if="stepActive === 1" @setCurrentStep="setCurrentStep" @fillAcademico="fillCreateAcademico"/>
+    <SecondStep v-if="stepActive === 2" @setCurrentStep="setCurrentStep" @fillAcademico="fillCreateAcademico"/>
+    <ThirdStep v-if="stepActive === 3" @setCurrentStep="setCurrentStep" @fillAcademico="fillUpdateAcademico"/>
   </div>
 </template>
 
