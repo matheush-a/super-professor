daac4cff5bf80706d3c79b01adeaebfb701dca25
matheush-a <mathzalmeida@gmail.com>
2024-02-07 12:08:23 -0300

    feat: componentes botão cta e mensagem erro

---
+++ frontend/src/components/CtaButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    @click="setCurrentStep"
+  >
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CtaButton',
+  props: {
+    text: String,
+    nextStep: Number,
+  },
+  methods: {
+    setCurrentStep() {
+      this.$emit('setCurrentStep', this.nextStep);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  margin-top: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #00be4b;
+  width: 100%;
+  height: 55px;
+  font-size: 20px;
+  font-weight: 700;
+  border-radius: 12px;
+  user-select: none;
+  transition: all 1s;
+  cursor: pointer;
+
+  &:hover {
+    background: #03682b;
+  }
+}
+</style>
---
+++ frontend/src/components/ErrorSpan.vue
@@ -0,0 +1,20 @@
+<template>
+  <span v-show="message"> 
+    {{message}}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  span {
+    color: rgb(187, 0, 0);
+    font-weight: 600;
+  }
+</style>
